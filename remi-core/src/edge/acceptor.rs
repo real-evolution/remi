9202af463de1350052c4b8acd3503e061443b0f7
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-15 02:40:22 +0300

    ref!: rewrite `Acceptor` trait to use `poll_*` instead of async trait

--- remi-core/src/edge/acceptor.rs
+++ remi-core/src/edge/acceptor.rs
@@ -1,10 +1,29 @@
-use crate::error::RemiResult;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
 
 use super::connection::Connection;
 
+/// An enum to represent the state of an acceptor.
+#[derive(Debug, Default)]
+pub enum AcceptorState {
+    #[default]
+    Stopped,
+    Running,
+}
+
 /// A trait to represent a connection acceptor.
-#[crate::async_trait]
-pub trait Acceptor<C: Connection> {
-    /// Accepts a new connection.
-    async fn next(&self) -> RemiResult<C>;
+pub trait Acceptor {
+    type Conn: Connection;
+    type Error;
+
+    // Polls for a new connection.
+    fn poll_accept(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<Self::Conn, Self::Error>>>;
+
+    /// Gets the current state of the acceptor.
+    fn state(&self) -> AcceptorState;
 }
--- remi-core/src/edge/mod.rs
+++ remi-core/src/edge/mod.rs
@@ -1,5 +1,6 @@
 mod acceptor;
 mod connection;
+mod mux;
 
-pub use acceptor::Acceptor;
+pub use acceptor::{Acceptor, AcceptorState};
 pub use connection::{Connection, Frame, FramedConnection, StreamConnection};
