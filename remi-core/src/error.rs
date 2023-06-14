c703173e19427a2417e4ff01b1e212f71882a894
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-14 21:55:11 +0300

    feat: add an error variant to represent unknown errors

--- remi-core/src/error.rs
+++ remi-core/src/error.rs
@@ -6,6 +6,9 @@ pub type RemiResult<T> = Result<T, RemiError>;
 /// A type to rerpesent errors returned by remi.
 #[derive(Error, Debug)]
 pub enum RemiError {
+    #[error("unknown error: {0}")]
+    Unknown(#[from] anyhow::Error),
+
     /// An error returned by the underlying transport.
     #[error("i/o error: {0}")]
     Io(#[from] std::io::Error),
