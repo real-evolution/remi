5bd9c9edb9e0e1e3548cece91079aa255454f72c
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-18 22:00:29 +0300

    feat: add error variants to be returned when invalid data is detected

--- remi-core/src/error.rs
+++ remi-core/src/error.rs
@@ -6,10 +6,19 @@ pub type RemiResult<T> = Result<T, RemiError>;
 /// A type to rerpesent errors returned by remi.
 #[derive(Error, Debug)]
 pub enum RemiError {
+    /// An unknown error occurred.
     #[error("unknown error: {0}")]
     Unknown(#[from] anyhow::Error),
 
     /// An error returned by the underlying transport.
     #[error("i/o error: {0}")]
     Io(#[from] std::io::Error),
+
+    /// An error returned when protocol-violating data is detected.
+    #[error("invalid format: {0}")]
+    InvalidFormat(&'static str),
+
+    /// An error returned when invalid frames are detected.
+    #[error("invalid frame: {0}")]
+    InvalidFrame(&'static str),
 }
