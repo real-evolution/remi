97c1c5356beabaa35fff799ab1e8535d6ebd8c3e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-10 04:22:21 +0300

    ref!: start over in `remi-rsocket`

--- remi-rsocket/src/edge/stream/frame.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug)]
-#[repr(transparent)]
-pub struct RSocketFrame(pub(crate) rsocket_rust::frame::Frame);
-
-impl From<rsocket_rust::frame::Frame> for RSocketFrame {
-    fn from(input: rsocket_rust::frame::Frame) -> Self {
-        Self(input)
-    }
-}
--- remi-rsocket/src/edge/stream/mod.rs
+++
@@ -1,7 +0,0 @@
-mod adapter;
-mod codec;
-mod frame;
-
-pub use adapter::RSocketStreamAdapter;
-pub use codec::RSocketStreamFrameCodec;
-pub use frame::RSocketFrame;
