b0ef2569a73d709e1f27dabc8890c6e74aa57628
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-11 18:21:46 +0300

    ref!: merge request and response into a single struct

--- remi-core/src/message/mod.rs
+++ remi-core/src/message/mod.rs
@@ -1,5 +1,39 @@
-mod request;
-mod response;
+/// A type to represent a message.
+///
+/// # Type Parameters
+/// - `M`: The type of the message metadata.
+/// - `B`: The type of the message body.
+pub struct Message<M, B> {
+    meta: M,
+    data: B,
+}
 
-pub use request::Request;
-pub use response::Response;
+impl<M, B> Message<M, B> {
+    /// Creates a new message.
+    ///
+    /// # Parameters
+    /// - `meta`: The metadata.
+    /// - `data`: The body.
+    #[inline]
+    pub const fn new(meta: M, data: B) -> Self {
+        Self { meta, data }
+    }
+
+    /// Returns a reference to the metadata.
+    #[inline]
+    pub const fn metadata(&self) -> &M {
+        &self.meta
+    }
+
+    /// Returns a reference to the body.
+    #[inline]
+    pub const fn data(&self) -> &B {
+        &self.data
+    }
+
+    /// Deconstructs the message into its metadata and body pair.
+    #[inline]
+    pub fn split(self) -> (M, B) {
+        (self.meta, self.data)
+    }
+}
---
+++ remi/src/lib.rs
@@ -0,0 +1,1 @@
+pub use remi_core as core;
