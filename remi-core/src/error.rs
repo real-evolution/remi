a293db832a3f522349a0b1a1ab2d0315dc99848b
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-12 23:36:46 +0300

    ref!: remove irrelevant error variants

--- remi-core/src/error.rs
+++ remi-core/src/error.rs
@@ -6,19 +6,5 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// A type to rerpesent errors returned by remi.
 #[derive(Error, Debug)]
 pub enum Error {
-    /// An unknown error occurred.
-    #[error("unknown error: {0}")]
-    Unknown(#[from] Box<dyn std::error::Error + Send + Sync>),
 
-    /// An error returned by the underlying transport.
-    #[error("i/o error: {0}")]
-    Io(#[from] std::io::Error),
-
-    /// An error returned when protocol-violating data is detected.
-    #[error("invalid format: {0}")]
-    InvalidFormat(&'static str),
-
-    /// An error returned when invalid frames are detected.
-    #[error("invalid frame: {0}")]
-    InvalidFrame(&'static str),
 }
