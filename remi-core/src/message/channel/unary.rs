1142a710152f75095e10272ae1afe28c23171530
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-12 04:23:43 +0300

    feat: implement delegation methods for unary channel

--- remi-core/src/message/channel/unary.rs
+++ remi-core/src/message/channel/unary.rs
@@ -2,9 +2,33 @@
 #[derive(Debug)]
 pub struct UnaryProducer<T>(kanal::OneshotAsyncSender<T>);
 
+impl<T> UnaryProducer<T> {
+    /// Sends a value to the consumer, consuming this sender.
+    ///
+    /// The value is returned back if the value could not be sent.
+    ///
+    /// # Parameters
+    /// - `value`: The value to send.
+    #[inline]
+    pub async fn send(self, value: T) -> Result<(), T> {
+        self.0.send(value).await
+    }
+}
+
 /// A oneshot channel consumer side.
 #[derive(Debug)]
 pub struct UnaryConsumer<T>(kanal::OneshotAsyncReceiver<T>);
+
+impl<T> UnaryConsumer<T> {
+    /// Receives a value from the producer.
+    ///
+    /// # Returns
+    /// The received value.
+    #[inline]
+    pub async fn recv(self) -> Option<T> {
+        self.0.recv().await.ok()
+    }
+}
 
 #[inline]
 #[must_use]
