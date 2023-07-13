31b8fb1933d71872baacc28a0912060e9ea9296f
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-13 11:29:29 +0300

    ref!: update hello world example to include a basic accepting server

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -1,5 +1,12 @@
+use remi::core::io::AcceptorItem;
+use remi_tcp::TcpAcceptor;
+use tower::service_fn;
+
 #[tokio::main]
 async fn main() {
-    let acceptor = remi_tcp::TcpAcceptor::bind("127.0.0.1:3434").await.unwrap();
-    remi::serve(acceptor).await.unwrap();
+    let acceptor = TcpAcceptor::bind("127.0.0.1:3434").await.unwrap();
+    let make_svc = service_fn(|_: &AcceptorItem<TcpAcceptor>| async { Ok(()) });
+    let server = remi_rsocket::RSocket::new();
+
+    remi::serve(acceptor, make_svc, server).await.unwrap();
 }
