d3c9b884c8d59e41966ab95322a027936165b4fe
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-14 21:12:36 +0300

    feat!: make distinct traits for server and its instances

--- remi-core/src/server/service/mod.rs
+++ remi-core/src/server/service/mod.rs
@@ -1,9 +1,30 @@
 mod streaming;
 mod unary;
 
-pub use streaming::{
-    ClientStreamingService,
-    ServerStreamingService,
-    StreamingService,
-};
-pub use unary::UnaryService;
+use tower::{MakeService, Service};
+
+pub use self::streaming::*;
+pub use self::unary::*;
+
+/// A trait to represent services that create servers that produce types that
+/// implement [`Instance<Svc>`] trait.
+pub trait Server<Conn, Svc>: MakeService<Conn, Svc>
+where
+    Self::Service: Instance<Svc>,
+{
+}
+
+/// A trait to represent types that can run passed services.
+pub trait Instance<Svc>: Service<Svc, Response = ()> + Send + 'static {}
+
+impl<M, S, Conn, Svc> Server<Conn, Svc> for M
+where
+    M: MakeService<Conn, Svc, Service = S>,
+    S: Instance<Svc>,
+{
+}
+
+impl<S, Conn> Instance<Conn> for S where
+    S: Service<Conn, Response = ()> + Send + 'static
+{
+}
