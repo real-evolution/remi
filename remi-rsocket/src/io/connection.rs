1c6b879ac9ced61af402aa9b431b187f9b8b4ace
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-14 21:14:06 +0300

    ref!: make `Connection<T>` a proxy type for `Framed`

--- remi-rsocket/src/io/connection.rs
+++ remi-rsocket/src/io/connection.rs
@@ -1,32 +1,45 @@
-use std::ops::{Deref, DerefMut};
-
+use bytes::BytesMut;
+use rsocket_proto::frame::Frame;
 use rsocket_proto::io::codec::FrameCodec;
 use tokio::io::{AsyncRead, AsyncWrite};
-use tokio_util::codec::Framed;
+use tokio_util::codec::{Decoder, Encoder, Framed};
 
 #[derive(Debug)]
-pub struct Connection<T, C>(Framed<T, C>);
+pub struct Connection<T>(Framed<T, FrameCodec>);
 
-impl<T, C> Connection<T, C>
+impl<T> Connection<T>
 where
     T: AsyncRead + AsyncWrite + Unpin,
 {
     #[inline]
-    pub fn new(inner: T, codec: C) -> Self {
-        Self(Framed::new(inner, codec))
-    }
-
-    #[inline]
-    pub fn new_raw(inner: T) -> Connection<T, FrameCodec> {
-        Connection::new(inner, FrameCodec::default())
+    pub fn new(inner: T) -> Self {
+        Self(Framed::new(inner, FrameCodec::default()))
     }
 }
 
-impl<T, C> Deref for Connection<T, C> {
-    type Target = Framed<T, C>;
+impl<T> Decoder for Connection<T> {
+    type Error = crate::Error;
+    type Item = Frame;
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+    #[inline]
+    fn decode(
+        &mut self,
+        src: &mut BytesMut,
+    ) -> Result<Option<Self::Item>, Self::Error> {
+        self.0.codec_mut().decode(src).map_err(Into::into)
+    }
+}
+
+impl<T> Encoder<Frame> for Connection<T> {
+    type Error = crate::Error;
+
+    #[inline]
+    fn encode(
+        &mut self,
+        item: Frame,
+        dst: &mut BytesMut,
+    ) -> Result<(), Self::Error> {
+        self.0.codec_mut().encode(item, dst).map_err(Into::into)
     }
 }
 
